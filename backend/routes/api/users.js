f78e39a63774f010750f504d403be2295652a9af
jomondo <joanmodaw@gmail.com>
2018-09-22 19:56:12 +0200

    terminar sign-up y sign-in + encriptacion passwd con bycript

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var router = require('express').Router();
 var User = mongoose.model('User');
+var bcrypt = require('bcrypt-nodejs');
 
 // return a list of users
 router.get('/', function(req, res, next) {
@@ -29,7 +30,7 @@ router.get('/:id', function(req, res, next) {
 
 });
 
-//Sign up
+//Sign up manual
 router.post('/register', function(req, res, next) {
   //console.log("arriba a sign-up");
   let param = req.body;
@@ -40,7 +41,7 @@ router.post('/register', function(req, res, next) {
     let user = new User();
 
     user.user = param.user;
-    user.name = "";
+    user.name = param.user;
 		user.surname = "";
     user.email = param.email;
     user.password1 = param.password1;
@@ -61,7 +62,28 @@ router.post('/register', function(req, res, next) {
 				return res.status(200).send({message:'User already exist'});
 
 			}else{
-        return res.status(200).send({message:'Puede registrarse'});
+        //encriptacion password
+        bcrypt.hash(param.password1, null, null, (err, hash) => {
+					user.password = hash;
+					user.save((err,  userStored) => {
+						if (err) return res.status(500).send({message:'Error to save user'});
+						//console.log(userStored);
+						if (userStored) {
+							res.status(200).send({
+								user: userStored
+
+							});
+						}else{
+							res.status(404).send({
+								message: "Don't register user"
+
+							});
+
+            }
+            
+          });
+          
+				});
         
       }
       
@@ -73,5 +95,48 @@ router.post('/register', function(req, res, next) {
   }
 
 });
+
+//sign-in manual
+router.post('/login', function(req, res, next) {
+  var values = req.body;
+	var email = values.email;
+	var password = values.password;
+
+  if(!email){
+    return res.status(422).json({errors: {email: "Please, write your email"}});
+
+  }
+
+  if(!password){
+    return res.status(422).json({errors: {password: "Please, write your password"}});
+
+  }
+
+	User.findOne( { email:email }, ( err, users ) => {
+		if (err) {
+			return res.status(500).send({message:'Error petition user'});
+
+		}
+
+		if (users) {
+			bcrypt.compare(password, users.password, (err, check) => {
+				if (check) {
+          return res.status(200).send({message:'Password correct'});
+
+				}else{
+					return res.status(404).send({message:'Password incorrect'});
+
+				}
+			});
+
+		}else{
+			return res.status(404).send({message:'User not exist'});
+
+		}
+
+  });
+  
+});
+
 
 module.exports = router;
