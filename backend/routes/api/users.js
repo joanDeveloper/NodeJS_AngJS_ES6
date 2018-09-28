7816d04481027c4ba0743737bd8f5b0a7d0e07ea
jomondo <joanmodaw@gmail.com>
2018-09-28 10:50:27 +0200

    resubir con los cambios buenos

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,7 +7,6 @@ var jwt = require('../../utils/jwt');
 var passport = require('passport');
 
 // return a list of users
-<<<<<<< HEAD
 router.get('/', md_auth.ensureAuth ,function(req, res, next) {
   //console.log(res);
   //console.log("hola user");
@@ -15,14 +14,6 @@ router.get('/', md_auth.ensureAuth ,function(req, res, next) {
   User.findOne( { '_id':req.user.sub }, ( err, users ) => {
 		if (err) {
 			return res.status(500).send({message:'Error petition user'});
-=======
-router.get('/', md_auth.ensureAuth, function (req, res, next) {
-  //console.log(res);
-  //console.log("hola user");
-  User.find().then(function (user) {
-    console.log(user);
-    return res.json({ user: user });
->>>>>>> 94665c6825c088ec2229516d45e76a895f967311
 
 		}
 
@@ -80,13 +71,9 @@ router.post('/register', function (req, res, next) {
     user.date_expiration = "";
     user.ubication = "";
 
-<<<<<<< HEAD
     //falta comparar el password 1 amb el 2
     User.find({ $or: [{email:user.email.toLowerCase()},{user:user.user.toLowerCase()} ]})
     .exec((err, users) => {
-=======
-    User.find({ $or: [{ email: user.email.toLowerCase() }, { user: user.user.toLowerCase() }] }).exec((err, users) => {
->>>>>>> 94665c6825c088ec2229516d45e76a895f967311
       if (err) {
         return res.status(500).send({ message: 'Error petition user' });
 
@@ -98,7 +85,6 @@ router.post('/register', function (req, res, next) {
       } else {
         //encriptacion password
         bcrypt.hash(param.password1, null, null, (err, hash) => {
-<<<<<<< HEAD
 					user.password = hash;
 					user.save((err,  userStored) => {
 						if (err) return res.status(500).send({message:'Error to save user'});
@@ -108,22 +94,6 @@ router.post('/register', function (req, res, next) {
               
 						}else{
 							res.status(404).send({message: "Don't register user"});
-=======
-          user.password = hash;
-          user.save((err, userStored) => {
-            if (err) return res.status(500).send({ message: 'Error to save user' });
-            //console.log(userStored);
-            if (userStored) {
-              res.status(200).send({
-                user: userStored
-
-              });
-            } else {
-              res.status(404).send({
-                message: "Don't register user"
-
-              });
->>>>>>> 94665c6825c088ec2229516d45e76a895f967311
 
             }
 
@@ -143,17 +113,10 @@ router.post('/register', function (req, res, next) {
 });
 
 //sign-in manual
-<<<<<<< HEAD
 router.post('/login', function(req, res, next) {
   var values = req.body.user;
 	var email = values.email;
 	var password = values.password;
-=======
-router.post('/login', function (req, res, next) {
-  var values = req.body;
-  var email = values.email;
-  var password = values.password;
->>>>>>> 94665c6825c088ec2229516d45e76a895f967311
 
   if (!email) {
     return res.status(422).json({ errors: { email: "Please, write your email" } });
@@ -175,24 +138,10 @@ router.post('/login', function (req, res, next) {
       bcrypt.compare(password, users.password, (err, check) => {
         if (check) {
           //return res.status(200).send({message:'Password correct'});
-<<<<<<< HEAD
           return res.status(200).send({
             token: jwt.create_token( users )
 
           });
-=======
-          if (values.get_token) {
-            return res.status(200).send({
-              token: jwt.create_token(users)
-
-            });
-
-          } else {
-            users.password = undefined;
-            return res.status(200).send({ users });
-
-          }
->>>>>>> 94665c6825c088ec2229516d45e76a895f967311
 
         } else {
           return res.status(404).send({ message: 'Password incorrect' });
@@ -218,14 +167,8 @@ router.get('/auth/googleplus', passport.authenticate('google', {
 );
 router.get('/auth/googleplus/callback',
   passport.authenticate('google', {
-<<<<<<< HEAD
    successRedirect : 'http://localhost:3000/api/category',
    failureRedirect: '/' }));
-=======
-    successRedirect: 'http://localhost:8081',
-    failureRedirect: '/'
-  }));
->>>>>>> 94665c6825c088ec2229516d45e76a895f967311
 
 router.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email', 'public_profile']}));
 router.get('/auth/facebook/callback',
